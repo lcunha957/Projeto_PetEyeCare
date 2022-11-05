a6f2623403634f43d5b1c6bbc8d5b28e711c8b47
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-05 19:38:12 -0300

    conexao com a bib okhttp3.
    .

--- NodeMCU_adaptado_esp32cam/app/src/main/java/com/example/nodemcu_adaptado_esp32cam/Conexao.java
+++ NodeMCU_adaptado_esp32cam/app/src/main/java/com/example/nodemcu_adaptado_esp32cam/Conexao.java
@@ -1,11 +1,26 @@
 package com.example.nodemcu_adaptado_esp32cam;
 
+import java.io.IOException;
+
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Conexao {
 
     public static String getDados(String urlUsuario){
-        OkHttpClient client = new OkHttpClient;
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(urlUsuario)
+                .build();
+
+        try{
+            Response response = client.newCall(request).execute();
+            return  response.body().string();
+        } catch (IOException erro) {
+            return erro.toString();
+        }
 
     }
 }
