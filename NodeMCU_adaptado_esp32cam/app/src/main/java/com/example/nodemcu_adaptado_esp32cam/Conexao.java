504272dea0b2c9ad64a600cd9621d884a927de7d
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-05 06:43:32 -0300

    parei em 13min22s do video 3
    FAZER: pegar a importação da biblioteca okHttp3

---
+++ NodeMCU_adaptado_esp32cam/app/src/main/java/com/example/nodemcu_adaptado_esp32cam/Conexao.java
@@ -0,0 +1,11 @@
+package com.example.nodemcu_adaptado_esp32cam;
+
+import okhttp3.OkHttpClient;
+
+public class Conexao {
+
+    public static String getDados(String urlUsuario){
+        OkHttpClient client = new OkHttpClient;
+
+    }
+}
--- NodeMCU_adaptado_esp32cam/app/src/main/java/com/example/nodemcu_adaptado_esp32cam/MainActivity.java
+++ NodeMCU_adaptado_esp32cam/app/src/main/java/com/example/nodemcu_adaptado_esp32cam/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.nodemcu_adaptado_esp32cam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLed1, btnLed2, btnLed3, btnTodos;
+    private TextView txtLdr;
+    private EditText txtResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnLed1 = (Button) findViewById(R.id.btnLed1);
+        btnLed2 = (Button) findViewById(R.id.btnLed2);
+        btnLed3 = (Button) findViewById(R.id.btnLed3);
+        btnTodos = (Button) findViewById(R.id.btnTodos);
+        txtLdr = (TextView) findViewById(R.id.txtLdr);
+        txtResultado =(EditText) findViewById(R.id.txtResultado);
+
+
     }
 }
