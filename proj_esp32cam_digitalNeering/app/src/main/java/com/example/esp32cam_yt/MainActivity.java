fb7c1e8788ad92095f324f0ce96beb453884ce54
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-05 23:25:39 -0300

    final do projeto digitalneering
    .

--- proj_esp32cam_digitalNeering/app/src/main/java/com/example/esp32cam_yt/MainActivity.java
+++ proj_esp32cam_digitalNeering/app/src/main/java/com/example/esp32cam_yt/MainActivity.java
@@ -2,21 +2,33 @@ package com.example.esp32cam_yt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnip;
-    private EditText edit_ip_address;
+    private Button enter_ip;
+    private EditText ip;
+    public static String ip_address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnip = (Button) findViewById(R.id.btnip);
-        edit_ip_address = (EditText) findViewById(R.id.edit_ip_address);
+        enter_ip = (Button) findViewById(R.id.btnip);
+        ip = (EditText) findViewById(R.id.edit_ip_address);
+
+        enter_ip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ip_address = ip.getText().toString();
+             Intent intent = new Intent(MainActivity.this, activity_2.class);
+             startActivity(intent);
+;            }
+        });
 
     }
 }
