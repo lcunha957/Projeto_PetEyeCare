39278e9c0d91d4152509f9371f494ce057e80fae
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-11-12 12:08:33 -0300

    ,
    .

--- proj_peteyecare_app/app/src/main/java/com/example/peteyecareapp/activities/LogarActivity.java
+++ proj_peteyecare_app/app/src/main/java/com/example/peteyecareapp/activities/LogarActivity.java
@@ -2,9 +2,12 @@ package com.example.peteyecareapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.peteyecareapp.R;
 
@@ -30,6 +33,42 @@ public class LogarActivity extends AppCompatActivity {
         btnEsqueceuASenhaLogin = (Button) findViewById(R.id.btnEsqueceuASenhaLogin);
         btnLimparLogin = (Button) findViewById(R.id.btnLimparLogin);
 
+
+        btnEntrarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLogar = new Intent(LogarActivity.this, MonitoramentoActivity.class);
+                Toast.makeText(LogarActivity.this, "Veja os nossos sensores na tela de monitoramento", Toast.LENGTH_LONG).show();
+                startActivity(intentLogar);
+            }
+        });
+
+        btnEsqueceuASenhaLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent esqueceuASenha = new Intent(LogarActivity.this, RestauraSenhaActivity.class);
+                Toast.makeText(LogarActivity.this, "Veja o processo de restaurar a senha", Toast.LENGTH_LONG).show();
+                startActivity(esqueceuASenha);
+            }
+        });
+
+      btnCadastrarLogin.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent cadastroulogin = new Intent(LogarActivity.this, CadastrarActivity.class);
+              Toast.makeText(LogarActivity.this, "Realize o seu cadastro na tela de cadastro", Toast.LENGTH_SHORT).show();
+              startActivity(cadastroulogin);
+          }
+      });
+
+      btnLimparLogin.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Toast.makeText(LogarActivity.this, "Os campos do login serão limpos", Toast.LENGTH_LONG).show();
+              edtTextEmail.setText("");
+              edtTextSenha.setText("");
+          }
+      });
 
     }
 }
